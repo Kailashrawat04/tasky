87379e7d7521f9e2a4ea14204d25d325f0d2ad38
Kailash Rawat <kailash2005rawat@gmail.com>
2025-04-07 15:30:15 +0530

    update files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import{ BrowserRouter as Router,
+   Routes,
+   Route }
+    from 'react-router-dom'
 
-function App() {
+const App = () => {
   return (
-    <div className="">
-    Shail Rawat
+    <div >
+    <Router>
+       <Routes>
+        <Route path='"/Login"' element={<Login />} />
+        <Route path='"/signup"' element={<Signup />} />
+
+        {/* Admin Routes */}
+        
+        <Route element={<privateRoute allowedRoles={['admin']} />}>
+          <Route path='/admin/manage-tasks' element={<ManageTasks />} />
+          <Route path='/admin/create-task' element={<CreateTask />} />
+        </Route>
+
+       </Routes>
+    </Router>
     </div>
   )
 }
