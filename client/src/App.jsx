806752ed5ccaee16243345c9a6aa428a5713520e
Kailash Rawat <kailash2005rawat@gmail.com>
2025-04-08 16:24:47 +0530

    uppdate it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,23 +2,42 @@ import React from 'react'
 import{ BrowserRouter as Router,
    Routes,
    Route }
-    from 'react-router-dom'
+from 'react-router-dom'
+import Login from './pages/Auth/Login'
+import Signup from './pages/Auth/Signup'
+import Dashboard from './pages/admin/Dashboard'
+import ManageTasks from './pages/admin/ManageTasks'
+import CreateTask from './pages/admin/CreateTask'
+import ManageUsers from './pages/admin/ManageUsers'
+import UserDashboard from './pages/User/UserDashboard'
+import MyTasks from './pages/User/MyTasks'
+import ViewTaskDetails from './pages/User/ViewTaskDetails'
+import PrivateRoute from './routes/PrivateRoute'
+
 
 const App = () => {
   return (
     <div >
     <Router>
        <Routes>
-        <Route path='"/Login"' element={<Login />} />
-        <Route path='"/signup"' element={<Signup />} />
-
+       
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         {/* Admin Routes */}
         
-        <Route element={<privateRoute allowedRoles={['admin']} />}>
-          <Route path='/admin/manage-tasks' element={<ManageTasks />} />
-          <Route path='/admin/create-task' element={<CreateTask />} />
+        <Route element={<PrivateRoute allowedRoles={['admin']} />}>
+          <Route path='/admin/dashboard' element={<Dashboard />} />
+          <Route path="/admin/tasks" element={<ManageTasks/>} />
+          <Route path='/admin/create-task' element={<CreateTask/>} />
+          <Route path='/admin/users' element={<ManageUsers />} />
         </Route>
 
+        {/* User Routes */}
+        <Route element={<PrivateRoute allowedRoles={['user']} />}>
+          <Route path='/user/user-dashboard' element={<UserDashboard />} />
+          <Route path='/user/My-tasks' element={<MyTasks />} />
+          <Route path='/user/task-details/:id' element={<ViewTaskDetails />} />
+          </Route>
        </Routes>
     </Router>
     </div>
